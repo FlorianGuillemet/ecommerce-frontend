29735bc7328fedd69ca8d2e80de27fad54395d11
Florian <florian.guillemet@mythalesgroup.io>
2020-09-04 17:37:58 +0200

    Modifiy the html of the search component + redirectUrl

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  search(value: string): void {
+    this.router.navigateByUrl(`/search/${value}`);
   }
 
 }
